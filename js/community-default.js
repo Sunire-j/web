3cb2eb4ee8429a6bf000fa00609c423b5e7b3f43
Sunire <81051472+Sunire-j@users.noreply.github.com>
2023-09-11 15:58:08 +0900

    게시글 보기 완료+css반영(로고 수정 필요)

--- js/community-default.js
+++ js/community-default.js
@@ -1,7 +1,3 @@
-document.getElementById("post-button").addEventListener("click", function () {
-  window.location.href = "Community_posting.html";
-});
-
 document
   .getElementById("main-search")
   .addEventListener("keydown", function (e) {
